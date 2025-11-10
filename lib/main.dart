20245d8e2ef5ab331ad9dabd4ca5a94f8e6adc7e
Laboratory 07 <lab07@lab.mu.edu>
2025-11-10 15:23:42 +0200

    final submission

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,91 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(Quiz());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Quiz extends StatelessWidget {
+  const Quiz({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(home: QuizScreen());
+  }
+}
+
+class QuizScreen extends StatefulWidget {
+  const QuizScreen({super.key});
+
+  @override
+  State<QuizScreen> createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  String result = "❓";
+  int tea = 0;
+  int coffe = 0;
+
+  void voteTea() {
+    setState(() {
+      result = "🍵";
+      tea++;
+    });
+  }
+
+  void voteCoffe() {
+    setState(() {
+      result = "☕";
+      coffe++;
+    });
+  }
+
+  void reset() {
+    setState(() {
+      result = "❓";
+      tea = 0;
+      coffe = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(result, style: TextStyle(fontSize: 50)),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: voteTea,
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: Text('Vote Tea', style: TextStyle(fontSize: 8)),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: voteCoffe,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                  ),
+                  child: Text(
+                    "Vote Coffe",
+                    style: TextStyle(color: Colors.white, fontSize: 8),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: reset, child: Text("Reset")),
+            SizedBox(height: 20),
+            Text("Tea Votes: ${tea}", style: TextStyle(color: Colors.black)),
+            SizedBox(height: 10),
+            Text(
+              "Coffe Vote : ${coffe}",
+              style: TextStyle(color: Colors.black),
+            ),
+          ],
         ),
       ),
     );
